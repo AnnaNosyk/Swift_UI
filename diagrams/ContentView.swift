2f93244dba24741d26f00a52228f0c48308ae02f
Anna Nosyk <anna.nosyk89@gmail.com>
2023-04-18 17:19:33 +0200

    - Diagrams from Rectangle()

--- diagrams/ContentView.swift
+++ diagrams/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pickerItem = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+      
+            ZStack {
+                Color("background").edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Analytics").font(.system(size: 30, weight: .semibold, design: .monospaced))
+                    Picker(selection: $pickerItem, label:
+                            Text("")) {
+                        Text("2021")
+                        Text("2022")
+                        Text("2023")
+                    }.pickerStyle(.segmented).padding(.horizontal, 16)
+                    HStack() {
+                        DiagramView()
+                        DiagramView()
+                        DiagramView()
+                    }
+                  
+                  
+                }
+            }
     }
 }
 
+
+struct DiagramView: View {
+    var body: some View  {
+        VStack {
+            ZStack(alignment: .bottom) {
+                Rectangle().frame(width: 20, height: 300).foregroundColor(Color.white)
+                Rectangle().frame(width: 20, height: 200).foregroundColor(Color("firstItemColor"))
+            }.padding(.top, 16)
+            Text("11").padding(.top, 4)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
