51ba4e331f73f76e5cd3eee9749fa4374ebd8168
Anna Nosyk <anna.nosyk89@gmail.com>
2023-04-18 15:05:37 +0200

    Initial Commit

---
+++ diagrams/diagramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  diagramsApp.swift
+//  diagrams
+//
+//  Created by Anna Nosyk on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct diagramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
